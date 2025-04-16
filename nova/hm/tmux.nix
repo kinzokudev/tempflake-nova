7bb9155092a86ecde514465a93340d68a4c005eb
Kira Rene <kira@renec88.xyz>
2025-04-16 15:11:57 -0400

    chore(hm/tmux): use system clipboard for vi-mode copy
    Signed-off-by: Kira Rene <kira@renec88.xyz>

--- nova/hm/tmux.nix
+++ nova/hm/tmux.nix
@@ -46,7 +46,7 @@
 
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'wl-copy -n'
 
       set -g visual-activity off
       set -g visual-bell off
